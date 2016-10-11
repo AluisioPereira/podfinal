456263f3481e88c8bbce4e700d0c04920a7281be
aluisiopereira <aluisio1102@gmail.com>
2016-10-11 08:23:46 -0300

    novas mudanças

--- q3-pod/src/main/java/ifpb/pod/rmi/chat/shared/NotificationRepository.java
+++ q3-pod/src/main/java/ifpb/pod/rmi/chat/shared/NotificationRepository.java
@@ -5,11 +5,28 @@
  */
 package ifpb.pod.rmi.chat.shared;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
 /**
  *
  * @author ajp
  */
 public class NotificationRepository {
+
+    String driver = "org.postgresql.Driver";
+    String user = "postgres";
+    String senha = "12345";
+    String url = "jdbc:postgresql://localhost:5432/pod";
+    Connection con = null;
+    private static final Logger LOG = Logger.getLogger(NotificationRepository.class.getName());
+
+    public NotificationRepository() throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        con = (Connection) DriverManager.getConnection(url, user, senha);
+    }
 
     public void store(Notification n) {
 
--- q3-pod/src/main/java/ifpb/pod/rmi/chat/shared/SubscriberRepository.java
+++ q3-pod/src/main/java/ifpb/pod/rmi/chat/shared/SubscriberRepository.java
@@ -5,14 +5,32 @@
  */
 package ifpb.pod.rmi.chat.shared;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author ajp
  */
 public class SubscriberRepository {
 
-    public void store(String uuid, Subscriber sub) {
+    String driver = "org.postgresql.Driver";
+    String user = "postgres";
+    String senha = "12345";
+    String url = "jdbc:postgresql://localhost:5432/pod";
+    Connection con = null;
 
+    public SubscriberRepository() throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        con = (Connection) DriverManager.getConnection(url, user, senha);
+    }
+
+    public void store(String uuid, Subscriber sub) throws SQLException {
+        PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO Topic VALUES (?,?)");
+        preparedStatement.setString(1, uuid);
+        preparedStatement.setObject(2, sub);
     }
 
     public Subscriber find(String uuid) {
